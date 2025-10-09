b4357daa36e81552cb8bc76ae52313e65200f796
Chloe B <hellochloe1989@protonmail.com>
2025-10-09 17:23:11 +0200

    largeur similaire entre le popup de filtrage et de recherche en mode carte

--- frontend/components/viewer/Navbar.vue
+++ frontend/components/viewer/Navbar.vue
@@ -234,7 +234,7 @@
     />
   </Dialog>
   <Popover ref="searchOp">
-    <div class="flex flex-col gap-4 w-[25rem]">
+    <div class="flex flex-col gap-4 w-[25rem] md:w-[28rem]">
       <Tabs value="0">
         <TabList>
           <Tab value="0">
